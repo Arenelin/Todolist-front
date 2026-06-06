6057315360d43047c787c2bc5b71889933d0ae2a
nakmaykin <nakmaykin@sfxdx.com>
2026-06-06 13:02:56 +0700

    feat: removes unnecessary files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type {Metadata} from "next";
 import "./globals.css";
+import {ReactNode} from "react";
 
 export const metadata: Metadata = {
   title: "A-Todolist-n",
@@ -9,7 +10,7 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return (
     <html
